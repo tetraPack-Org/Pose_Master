71b36d0da5f5aaec54189653786c6c0e81fbf2fa
AbhiRam162105 <abhiyanampally@gmail.com>
2025-04-10 14:39:14 +0530

    secure false

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -54,7 +54,7 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      // secure: true,
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });

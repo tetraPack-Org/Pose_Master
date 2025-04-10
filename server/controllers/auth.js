ff7274ad160f14f5841cfa64bac21ee37bc8b804
AbhiRam162105 <abhiyanampally@gmail.com>
2025-04-10 14:27:25 +0530

    added secure serve for httpsr cookies

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -54,7 +54,7 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production", // Only use cookies over HTTPS in production
+      secure: true,
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });

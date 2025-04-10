7217a033b82e99498f2efd42a0ebbe464a00a3e5
AbhiRam162105 <abhiyanampally@gmail.com>
2025-04-10 14:09:27 +0530

    checking token auth error

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -77,6 +77,7 @@ export const login = async (req, res) => {
 export const getCurrentUser = async (req, res) => {
   try {
     const token = req.cookies.token;
+    console.log("request", req.cookies);
     if (!token) return res.status(401).json({ error: "Not authenticated" });
 
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
